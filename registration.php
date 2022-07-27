f26014978c3353965c8c9f766240fc89cda26382
MCYau <108783365+MCYau@users.noreply.github.com>
2022-07-28 02:39:37 +0800

    Add 'check duplicate username function' on registration page

--- registration.php
+++ registration.php
@@ -83,27 +83,31 @@ h3 {
         $password = stripslashes($_REQUEST['password']);
         $password = mysqli_real_escape_string($con, $password);
         $create_datetime = date("Y-m-d H:i:s");
-        $query    = "INSERT into `users` (username, password, email, create_datetime)
-                     VALUES ('$username', '" . md5($password) . "', '$email', '$create_datetime')";
-        $result   = mysqli_query($con, $query);
-        /* $checkDuplicateQuery = "SELECT * FROM `users` WHERE username='$username'";
-        $duplicateResult = mysqli_query($con, $checkDuplicateQuery);
-        $duplicateRows = mysqli_num_rows($duplicateResult); */
+        $checkDuplicateQuery = mysqli_query($con, "SELECT username from users where username='$username'");
+        $checkDuplicateRow = mysqli_num_rows($checkDuplicateQuery);
         
-        elseif ($result) {
+        if ($checkDuplicateRow == 1){
             echo "<div class='form'>
-                  <h3>You are registered successfully.</h3><br/>
-                  <p class='link'>Click here to <a href='login.php'>Login</a></p>
+                  <h3>Registration failed! Username already exist.</h3><br/>
+                  <p class='link'>Click here to <a href='registration.php'>Register again</a></p>
                   </div>";
-        } else {
-            if (errno()==1062){
-                echo "<script>alert('Username already exist.') ; window.location.href = 'registration.php'</script>";
+        }else{
+            $query    = "INSERT into `users` (username, password, email, create_datetime)
+                     VALUES ('$username', '" . md5($password) . "', '$email', '$create_datetime')";
+            $result   = mysqli_query($con, $query); 
+            if ($result) {
+                echo "<div class='form'>
+                      <h3>You are registered successfully.</h3><br/>
+                      <p class='link'>Click here to <a href='login.php'>Login</a></p>
+                      </div>";
+            } else {
+                echo "<div class='form'>
+                      <h3>Required fields are missing.</h3><br/>
+                      <p class='link'>Click here to <a href='registration.php'>registration</a> again.</p>
+                      </div>";
             }
-            echo "<div class='form'>
-                  <h3>Required fields are missing.</h3><br/>
-                  <p class='link'>Click here to <a href='registration.php'>registration</a> again.</p>
-                  </div>";
-        }
+        } 
+       
     } else {
 ?>
     <form class="form" action="" method="post">
